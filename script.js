c1ba8057036213e83a0b1c9b47243c87160fee0c
hyunnies95 <hyunnies95@gmail.com>
2021-06-29 13:29:28 +0500

    fixes for mobile screen

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
-// const body = document.body;
-// const header = document.querySelector('.header');
-// const menuBtn = document.querySelector('.nav__toggle');
-
-// function toggleMobileNav() {
-//   header.classList.toggle('mobile-nav--active');
-// }
-
-// function disableScroll() {
-//   body.classList.toggle('disable-scroll');
-// }
-
-// menuBtn.addEventListener('click', () => {
-//   toggleMobileNav();
-//   disableScroll();
-// });
+AOS.init()
 
 const hamburger = document.querySelector('.header__hamburger'),
       menu = document.querySelector('.header__links')
